1bb2ae2d73041f3cb45e70ca6a176d2c7f620b23
Samir <emaildesamir@gmail.com>
2020-03-28 23:55:57 -0300

    adicionando validações de rotas no backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 
 const OngController = require ('./controllers/OngController');
 const IncidentController = require ('./controllers/IncidentController');
@@ -9,14 +10,64 @@ const routes = express.Router();
 
 routes.post('/sessions', SessionController.create);
 
+
+/** ONG CONTROLLER START */
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
-routes.delete('/ongs/:id', OngController.delete);
 
-routes.get('/profile', ProfileController.index);
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required().min(12).max(13),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+}), OngController.create);
 
-routes.get('/incidents', IncidentController.index);
-routes.post('/incidents', IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete);
+routes.delete('/ongs/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}), OngController.delete);
+
+/** ONG CONTROLLER END */
+/** PROFILE CONTROLLER START */
+
+/** O Header é feito diferente pois é enviado varios headers que eu desconheço, então preciso validar somente o header que eu solicitei
+ * É feito dessa forma para que não seja bloqueado o restante dos header que a aplicação pede automaticamente
+*/
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+}), ProfileController.index);
+
+/** PROFILE CONTROLLER END */
+/** INCIDENT CONTROLLER START */
+
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number(),
+    })
+}), IncidentController.index);
+
+routes.post('/incidents', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),    
+    [Segments.BODY]: Joi.object().keys({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        value: Joi.number().required(),
+    })
+}), IncidentController.create);
+
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}), IncidentController.delete);
+
+/** INCIDENT CONTROLLER END */
 
 module.exports = routes;
